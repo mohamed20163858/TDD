40e202aaa5074247be0827f26e34c4320d3bde40
Mohamed Saleh <mohamed20163858@gmail.com>
2022-12-02 20:22:05 +0200

    Fix rubocop linters errors

--- solver.rb
+++ solver.rb
@@ -1,20 +1,25 @@
-class Solver 
-  def factorial(n)
-    return raise "invalid input you cannot enter a negative integer!" if n<0
-    result = 1 
-    (1..n).each { |i| result *= i } 
+class Solver
+  def factorial(num)
+    return raise 'invalid input you cannot enter a negative integer!' if num.negative?
+
+    result = 1
+    (1..num).each { |i| result *= i }
     result
   end
+
   def reverse(str)
-    return '' if str.length == 0
+    return '' if str.empty?
+
     result = ''
-    (0..str.length-1).reverse_each { |i| result += str[i] }
-    result 
-  end 
-  def fizzbuzz(n)
-    return 'fizzbuzz' if (n%3 == 0 and n%5 == 0) 
-    return 'fizz' if n%3 == 0
-    return 'buzz' if n%5 == 0
-    return n.to_s
+    (0..str.length - 1).reverse_each { |i| result += str[i] }
+    result
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? and (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,63 +1,87 @@
 require_relative '../solver'
-describe Solver do 
-  context "test solver class behaviour" do 
-    before(:all) do 
-      @solver = Solver.new #arrange 
+describe Solver do
+  context 'test solver class behaviour' do
+    before(:all) do
+      @solver = Solver.new # arrange
     end
-    it "test the existence of solver class" do 
-      expect(@solver).to be_instance_of(Solver) #assert
+    it 'test the existence of solver class' do
+      expect(@solver).to be_instance_of(Solver) # assert
     end
-    it "test the existence of method factorial" do 
-        expect(@solver).to respond_to(:factorial) #assert
+    it 'test the existence of method factorial' do
+      expect(@solver).to respond_to(:factorial) # assert
     end
-    it "test the existence of method reverse" do 
-        expect(@solver).to respond_to(:reverse) #assert
+    it 'test the existence of method reverse' do
+      expect(@solver).to respond_to(:reverse) # assert
     end
-    it "test the existence of method fizzbuzz" do 
-        expect(@solver).to respond_to(:fizzbuzz) #assert
+    it 'test the existence of method fizzbuzz' do
+      expect(@solver).to respond_to(:fizzbuzz) # assert
     end
-    it "test the factorial of base case 0" do 
+  end
+end
+
+describe Solver do
+  context 'test solver factorial method  behaviour' do
+    before(:all) do
+      @solver = Solver.new # arrange
+    end
+    it 'test the factorial of base case 0' do
       result = @solver.factorial(0)
       expect(result).to eq 1
     end
-    it "test the factorial of 5" do 
-        result = @solver.factorial(5)
-        expect(result).to eq 120
+    it 'test the factorial of 5' do
+      result = @solver.factorial(5)
+      expect(result).to eq 120
     end
-    it "test the factorial of 8" do 
-        result = @solver.factorial(8)
-        expect(result).to eq 40320
+    it 'test the factorial of 8' do
+      result = @solver.factorial(8)
+      expect(result).to eq 40_320
     end
-    it "test the factorial of negative number" do 
-        expect { @solver.factorial(-10) }.to raise_error("invalid input you cannot enter a negative integer!")
+    it 'test the factorial of negative number' do
+      expect { @solver.factorial(-10) }.to raise_error('invalid input you cannot enter a negative integer!')
     end
-    it "test  reverse using base case '' " do 
-        result = @solver.reverse('')
-        expect(result).to eq ''
-      end
-    it "test the reverse of word 'hello'" do 
+  end
+end
+
+describe Solver do
+  context 'test solver reverse method  behaviour' do
+    before(:all) do
+      @solver = Solver.new # arrange
+    end
+    it "test  reverse using base case '' " do
+      result = @solver.reverse('')
+      expect(result).to eq ''
+    end
+    it "test the reverse of word 'hello'" do
       result = @solver.reverse('hello')
       expect(result).to eq 'olleh'
     end
-    it "test the reverse of word 'mostafa saleh'" do 
-        result = @solver.reverse('mostafa saleh')
-        expect(result).to eq 'helas afatsom'
+    it "test the reverse of word 'mostafa saleh'" do
+      result = @solver.reverse('mostafa saleh')
+      expect(result).to eq 'helas afatsom'
     end
-    it "test the fizzbuzz of number 9" do 
-        result = @solver.fizzbuzz(9)
-        expect(result).to eq 'fizz'
+  end
+end
+
+describe Solver do
+  context 'test solver fizzbuzz method  behaviour' do
+    before(:all) do
+      @solver = Solver.new # arrange
     end
-    it "test the fizzbuzz of number 125" do 
-        result = @solver.fizzbuzz(125)
-        expect(result).to eq 'buzz'
+    it 'test the fizzbuzz of number 9' do
+      result = @solver.fizzbuzz(9)
+      expect(result).to eq 'fizz'
     end
-    it "test the fizzbuzz of number 75" do 
-        result = @solver.fizzbuzz(75)
-        expect(result).to eq 'fizzbuzz'
+    it 'test the fizzbuzz of number 125' do
+      result = @solver.fizzbuzz(125)
+      expect(result).to eq 'buzz'
     end
-    it "test the fizzbuzz of number 11" do 
-        result = @solver.fizzbuzz(11)
-        expect(result).to eq '11'
+    it 'test the fizzbuzz of number 75' do
+      result = @solver.fizzbuzz(75)
+      expect(result).to eq 'fizzbuzz'
+    end
+    it 'test the fizzbuzz of number 11' do
+      result = @solver.fizzbuzz(11)
+      expect(result).to eq '11'
     end
   end
 end
